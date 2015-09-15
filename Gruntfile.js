9a263141f6adb458c8557cf35daa8c188b5598de
jer0dh <jerod@jhtechservices.com>
2015-09-15 16:03:23 -0500

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,82 @@
+// See: http://24ways.org/2013/grunt-is-not-weird-and-hard/
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		uncss: {
+			dist: {
+				src: ['source.html'],
+				dest: 'working/tidy.css',
+				options: {
+					report: 'min' //optional: include to report savings
+				}
+			}
+
+		},
+		processhtml: {
+			dist: {
+				files: {
+					'preInlined.html' : ['source.html']
+				}
+			}
+		},
+		stripCssComments: {
+			dist: {
+				files: {
+					'working/verytidy.css': 'working/tidy.css'
+				}
+			}
+		},
+		replace: {
+			css: {
+				src: ['working/verytidy.css'],             // source files array (supports minimatch)
+				dest: 'working/veryverytidy.css',             // destination directory or file
+				replacements: [{
+					from: /^\s*$/gm,      // regex remove extra lines
+					to: ''
+				}]
+			},
+			html: {
+				src: ['preInlined.html'],
+				overwrite: true,
+				replacements: [{
+						from: '<table',
+						to: '<table border="0" cellpadding="0" cellspacing="0"'
+					}]
+			}
+		},
+		compress: {
+			main: {
+				options: {
+					archive: 'email.zip'
+				},
+				files: [
+					{src: ['images/*'], dest: '/', filter: 'isFile'}, // includes files in path
+					{src: ['email.html'], dest: '/'}
+				]
+			}
+		},
+		watch: {
+			js: {
+				files: ['source.html', 'my_styles.css'],
+				tasks: ['uncss', 'processhtml'],
+				options: {
+					spawn: false
+				}
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-uncss');
+	grunt.loadNpmTasks('grunt-processhtml');
+	grunt.loadNpmTasks('grunt-strip-css-comments');
+	grunt.loadNpmTasks('grunt-text-replace');
+	grunt.loadNpmTasks('grunt-contrib-compress')
+	grunt.registerTask('emptyFile', 'Removes Inlined file', function() {
+		grunt.file.write('email.html', '');
+	});
+	grunt.registerTask('default', ['uncss', 'stripCssComments','replace:css','processhtml', 'replace:html','emptyFile']);
+	grunt.registerTask('zip', ['compress']);
+};
---
+++ readme.md
@@ -0,0 +1,25 @@
+# Grunting the Email Campaign #
+
+This package is a starting point in developing a responsive HTML email for an email campaign.
+
+    * source.html - Here is where you type in the HTML for the email using the Zurb Ink framework
+    * my_styles.css - Contains your customized css styles
+    * \images - put all your images here
+    * \working - a directory for files that are created and destroyed during the grunt process..always safe to delete contents
+    * ink.css - Zurb Ink framework.  Replace this file with the latest version
+
+## Grunt commands ##
+The default task does it all.
+  1. It runs grunt-uncss which creates a \working\tidy.css file that only contains the css that was used.
+  2. Using stripcsscomments it creates \working\verytidy.css from \working\tidy.css where all comments are stripped out.
+  3. Using replace:css, it removes all the extra lines from \working\verytidy.css and creates working\veryverytidy.css.
+  4. Using processhtml, it puts veryverytidy.css into preInlined.html.
+  5. Using replace:html, it addes border=0 cellspacing=0 and cellpadding=0 to all <table> elements.
+  6. Finally, it clears out email.html to be used to copy and paste in the inlined product.
+
+## Steps to create an email ##
+1. Edit the source.html, add the images to the images directory unless you already have a complete url path for them.
+2. Test source.html using your local dev and make changes as needed.
+3. run the default grunt task
+4. Copy the preInlined.html file it creates and use Zurb Ink inliner to inline the styles.  Paste the product into the email.html file.  Unfortunately, there currently isn't a great inliner for the Windows machine so I still must copy&paste.
+5. run 'grunt zip' to zip up your images folder and the email.html file and send to EmailOnAcid or Litmus for testing.
